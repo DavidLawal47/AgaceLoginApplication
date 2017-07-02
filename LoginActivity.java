afd1bd38c02b506b390b6912b7c98a3d24fe6b1e
David Lawal <agacecornish@yahoo.com>
2017-07-02 08:17:54 -0400

    Add files via upload
    Source Code for AgaceLoginApp

---
+++ LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.agace.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by agace on 6/25/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
